8340668a6accb330bf03dfa4eb3561b2c889b0e0
Nikita Zdanovitch <nzdanovitch@gmail.com>
2021-08-05 00:52:50 -0300

    Command to create an A

--- src/playdate/xxx.rs
+++ src/playdate/xxx.rs
@@ -28,7 +28,7 @@ mod take_1 {
 }
 
 mod take_2 {
-    use bevy::prelude::*;
+    use bevy::{ecs::system::Command, prelude::*};
     use std::collections::HashMap;
 
     type ARef = String;
@@ -37,33 +37,61 @@ mod take_2 {
         Y(Y),
     }
 
-    #[derive(Clone)]
+    #[derive(Clone, Debug)]
     struct Y {
         a_ref: ARef,
         c: HashMap<SName, C>,
     }
 
+    #[derive(Clone, Debug)]
+    enum C {
+        Y(Y),
+        S(S),
+    }
+
     type SName = String;
     type SKind = String;
 
-    #[derive(Clone)]
+    #[derive(Clone, Debug)]
     struct S {
         name: SName,
         kind: SKind,
     }
 
-    impl S {
-        fn create(&self, root: Entity, world: &mut World) {
-            world.entity_mut(root).insert(self.clone());
+    struct CreateACommand {
+        a_ref: ARef,
+        root: Entity,
+    }
+
+    #[derive(Debug)]
+    enum CreateACommandError {
+        Fetch(Entity, FetchError),
+        Create(Entity, ACreateError),
+    }
+
+    impl Command for CreateACommand {
+        fn write(self: Box<Self>, world: &mut World) {
+            match fetch(&self.a_ref, world) {
+                Err(err) => {
+                    let mut errors = world
+                        .get_resource_mut::<Vec<CreateACommandError>>()
+                        .unwrap();
+                    errors.push(CreateACommandError::Fetch(self.root, err));
+                }
+                Ok(a) => match a.create(self.root, world) {
+                    Err(err) => {
+                        let mut errors = world
+                            .get_resource_mut::<Vec<CreateACommandError>>()
+                            .unwrap();
+                        errors.push(CreateACommandError::Create(self.root, err));
+                    }
+                    Ok(()) => {}
+                },
+            }
         }
     }
 
-    type XCreateError = String;
-
-    trait X {
-        fn create(&self, root: Entity, world: &mut World) -> Result<(), XCreateError>;
-    }
-
+    #[derive(Debug)]
     enum ACreateError {
         X(XCreateError),
         Y(YCreateError),
@@ -78,39 +106,34 @@ mod take_2 {
         }
     }
 
+    type XCreateError = String;
+
+    trait X {
+        fn create(&self, root: Entity, world: &mut World) -> Result<(), XCreateError>;
+    }
+
+    #[derive(Debug)]
     enum YCreateError {
         Fetch(FetchError),
         GetSs(GetSsError),
         CreatingRoot(Box<ACreateError>),
         NoCforS(S),
-        CreatingC(S, Entity, Box<Self>),
-        YFullfillS(Y, S),
-        SFullfillS(S, S),
+        CreatingC(S, Entity, CCreateError),
     }
 
     impl Y {
         fn create(&self, root: Entity, world: &mut World) -> Result<(), YCreateError> {
-            let a = fetch(&self.a_ref).map_err(YCreateError::Fetch)?;
-            world.entity_mut(root).insert(C::Y(self.clone()));
+            let a = fetch(&self.a_ref, world).map_err(YCreateError::Fetch)?;
+            world.entity_mut(root).insert(self.clone());
             a.create(root, world)
                 .map_err(|err| YCreateError::CreatingRoot(Box::new(err)))?;
             let ss = get_ss(root, world).map_err(YCreateError::GetSs)?;
             for (s_entity, s) in ss {
                 match self.c.get(&s.name) {
-                    Some(C::Y(y)) => {
-                        if !y_fullfills_s(y, &s.kind) {
-                            return Err(YCreateError::YFullfillS(y.clone(), s));
-                        }
-                        y.create(s_entity, world).map_err(|err| {
-                            YCreateError::CreatingC(s, s_entity, Box::new(err))
-                        })?;
+                    Some(c) => {
+                        c.create(s.clone(), s_entity, world)
+                            .map_err(|err| YCreateError::CreatingC(s, s_entity, err))?;
                     }
-                    Some(C::S(c_s)) => {
-                        if !s_fullfills_s(&c_s.kind, &s.kind) {
-                            return Err(YCreateError::SFullfillS(c_s.clone(), s.clone()));
-                        }
-                        c_s.create(s_entity, world);
-                    },
                     None => return Err(YCreateError::NoCforS(s)),
                 }
             }
@@ -118,23 +141,62 @@ mod take_2 {
         }
     }
 
-    #[derive(Clone)]
-    enum C {
-        Y(Y),
-        S(S),
+    #[derive(Debug)]
+    enum CCreateError {
+        YFullfillS(Y),
+        SFullfillS(S),
+        CreatingY(Box<YCreateError>),
     }
 
+    impl C {
+        fn create(&self, s: S, root: Entity, world: &mut World) -> Result<(), CCreateError> {
+            match self {
+                C::Y(y) => {
+                    if !y_fullfills_s(y, &s.kind) {
+                        return Err(CCreateError::YFullfillS(y.clone()));
+                    }
+                    y.create(root, world)
+                        .map_err(|err| CCreateError::CreatingY(Box::new(err)))?;
+                }
+                C::S(c_s) => {
+                    if !s_fullfills_s(&c_s.kind, &s.kind) {
+                        return Err(CCreateError::SFullfillS(c_s.clone()));
+                    }
+                    c_s.create(root, world);
+                }
+            }
+            Ok(())
+        }
+        fn fullfils_s(&self, s: S) -> Result<(), CCreateError> {
+            match self {
+                C::Y(y) if !y_fullfills_s(y, &s.kind) => Err(CCreateError::YFullfillS(y.clone())),
+                C::S(c_s) if !s_fullfills_s(&c_s.kind, &s.kind) => {
+                    Err(CCreateError::SFullfillS(c_s.clone()))
+                }
+                _ => Ok(()),
+            }
+        }
+    }
 
+    impl S {
+        fn create(&self, root: Entity, world: &mut World) {
+            world.entity_mut(root).insert(self.clone());
+        }
+    }
+
+    #[derive(Debug)]
     struct FetchError;
-    fn fetch(a_ref: &ARef) -> Result<A, FetchError> {
+    fn fetch(a_ref: &ARef, world: &World) -> Result<A, FetchError> {
         todo!()
     }
 
+    #[derive(Debug)]
     struct GetSsError;
     fn get_ss(root: Entity, world: &World) -> Result<Vec<(Entity, S)>, GetSsError> {
         todo!()
     }
 
+    #[derive(Debug)]
     struct SFullfilmentError;
     fn y_fullfills_s(y: &Y, s_kind: &SKind) -> bool {
         todo!()
